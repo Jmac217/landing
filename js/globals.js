43150b1e603b89de21b5bc14694ed5fb154f87f2
Jmac217 <jordanelder10@gmail.com>
2014-05-29 15:57:02 -0500

    comments everywhere

--- js/globals.js
+++ js/globals.js
@@ -1,5 +1,5 @@
 var notes_obj={page:0};
-var notes_page_cap=3;
+
 
 function post_notes(){
 
@@ -16,20 +16,10 @@ function post_notes(){
 }
 function notes_next_id(notes_obj){
 	//alert(notes_obj);
-	notes_obj.page++;
-	if(notes_obj.page>notes_page_cap){
-		notes_obj.page=0;
-	}
-	notes=todo.notes[notes_obj.page];
-	console.log(notes_obj.page);
+
 	//post_notes();
 }
 function notes_last_id(notes_obj){
-	notes_obj.page--;
-	if(notes_obj.page==-1){
-		notes_obj.page=notes_page_cap;
-	}
-	notes=todo.notes[notes_obj.page];
-	console.log(notes_obj.page);
+
 	//post_notes();
 }
